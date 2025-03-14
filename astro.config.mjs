5c74d06b2a15906264ed347d0d8f1f0b62d45930
danilo <sanfracaceresdanilo@gmail.com>
2025-03-14 16:48:14 -0300

    Revert "Eliminar imagen obsoleta, añadir componente QR y actualizar configuración del proyecto"
    This reverts commit ed94725a3ef81466303f83636bc97a1ab9c1b2a4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/danilo1992-sys/cv.github.io'
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
